15f6ba556ccf38f518895337a7259844084665cf
luis-guarnizo <gonzalo.guarnizo.garcia@gmail.com>
2022-12-27 02:36:56 -0500

    probando consultar

--- view/username/index.php
+++ view/username/index.php
@@ -1,63 +1,19 @@
 <?php
     require_once("c://xampp/htdocs/maxivideo/view/head/head.php");
     require_once("c://xampp/htdocs/maxivideo/controller/usernameController.php");
-    $obj = new usernameController();
-    //$rows = $obj->index();
 ?>
-<div class="mb-3">
-    <a href="create.php" class = "btn btn-primary">Alquilar película</a>
-</div>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php if($rows): ?>
-            <?php foreach($rows as $row): ?>
-                <tr>
-                    <th><?= $row["id"]?></th>
-                    <th><?= $row["nombre"]?></th>
-                    <th>
-                        <a href="show.php?id=<?= $row["id"]?>" class="btn btn-primary">Ver</a>
-                        <a href="edit.php?id=<?= $row["id"]?>" class="btn btn-success">Modificar</a>
-                        <!-- Button trigger modal -->
-                        <a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</a>
-                        
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">¿Desea eliminar el registro?</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Una vez eliminado no se podrá recuperar el registro
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <a href="delete.php?id=<?= $row["id"]?>" class="btn btn-danger">Eliminar</a>
-                                
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </th>
-                    
-                </tr>
-            <?php endforeach ?>
-        <?php else: ?>
-            <tr>
-                <td colspan="3" class="text-center">No hay Registros disponibles</td>
-            </tr>
-        <?php endif; ?>
-    </tbody>
-</table>
+    <h2>Ingrese la cedula del cliente para consultar</h2>
+    <form action="consult.php" method= "POST" autocomplete="off">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Cédula</label>
+            <input type="text" name="ident_cliente" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Consultar</button>
+        <a class="btn btn-danger" href="index.php">Cancelar</a>
+    </form>
+
 <?php
     require_once("c://xampp/htdocs/maxivideo/view/head/footer.php");    
 ?>
